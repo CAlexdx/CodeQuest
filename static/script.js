4914c14f53649797a47a98c338b2a194606bbc34
Calebe alves Câmara <minecraftxdxcalebe@gmail.com>
2026-03-06 13:10:19 -0300

    atua

---
+++ static/script.js
@@ -0,0 +1,18 @@
+function responder(opcao){
+
+let correta = "print()"
+
+if(opcao == correta){
+
+document.getElementById("resultado").innerText =
+"Resposta correta +10 XP"
+
+}
+else{
+
+document.getElementById("resultado").innerText =
+"Resposta errada"
+
+}
+
+}
